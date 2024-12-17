5e52d02a5cc96899940b7cb58d7705da718fe88d
AxelSevenS <spaghettiandmeatballs61@gmail.com>
2024-12-17 10:17:04 +0100

    feat(RGB): Add RGB class for NeoPixel control and integrate with sketch
    Co-authored-by: Guillaume <GuillaumePham@users.noreply.github.com>

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,12 +1,36 @@
+#include "RGB.cpp"
 #include "LCD.cpp"
-LCD lcd(0x27, 16, 2);
+
+RGB rgb;
+LCD lcd;
 
 void setup() {
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+	clock_prescale_set(clock_div_1);
+#endif
+
+	rgb.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+
 	lcd.begin ();
-	lcd.writeText (0, 0, "ricardo");
-	lcd.writeText (2, 1, "GIGACHAD");
+
+	// SetTheServo();
 }
 
 void loop() {
+	// Fill along the length of the strip in various colors...
+	rgb.colorWipe(rgb.color(255,   0,   0), 50); // Red
+	rgb.colorWipe(rgb.color(  0, 255,   0), 50); // Green
+	rgb.colorWipe(rgb.color(  0,   0, 255), 50); // Blue
 
+	// Do a theater marquee effect in various colors...
+	rgb.theaterChase(rgb.color(127, 127, 127), 50); // White, half brightness
+	rgb.theaterChase(rgb.color(127,   0,   0), 50); // Red, half brightness
+	rgb.theaterChase(rgb.color(  0,   0, 127), 50); // Blue, half brightness
+
+	rgb.rainbow(10);             // Flowing rainbow cycle along the whole strip
+	rgb.theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
+	//OpenTheGate();
+
+
+	lcd.scrollText ("un message bien long pour test si axel est un chad", 0, 200);
 }
